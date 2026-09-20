a77b2d28e1190ed9be1cd380a9f7f2f29fb5345b
Appa <1pewdiepieisbetter1@gmail.com>
2026-09-19 23:04:07 -0400

    Added distinct effect and expression

---
+++ src/main/java/org/appa/skippa/skript/elements/effects/EffDistinct.java
@@ -0,0 +1,69 @@
+package org.appa.skippa.skript.elements.effects;
+
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Example;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript.registration.SyntaxInfo;
+import org.skriptlang.skript.registration.SyntaxRegistry;
+
+import java.util.Arrays;
+
+@Name("Distinct")
+@Description({"Removes the duplicate values out of a variable."})
+@Example("""
+        on script load:
+            set {_t::*} to 1, 2, 2, 3, 3
+            make {_t::*} distinct
+            broadcast {_t::*} # 1,2,3
+       """)
+@Since("1.0")
+public class EffDistinct extends Effect {
+
+    public static void register(SyntaxRegistry registry) {
+        registry.register(
+                SyntaxRegistry.EFFECT,
+                SyntaxInfo.builder(EffDistinct.class)
+                    .supplier(EffDistinct::new)
+                    .addPatterns(
+                        "make %objects% distinct",
+                        "make %objects% unique"
+                    )
+                    .build()
+        );
+    }
+
+    private Expression<?> objects;
+
+
+    @Override
+    public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        objects = expressions[0];
+
+        return objects.acceptChange(Changer.ChangeMode.SET) != null;
+    }
+
+
+    @Override
+    protected void execute(Event event) {
+        Object[] values = objects.getArray(event);
+
+        Object[] distinct = Arrays.stream(values)
+                .distinct()
+                .toArray();
+
+        objects.change(event, distinct, Changer.ChangeMode.SET);
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean debug) {
+        return "make" + objects.toString(event, debug) + "distinct";
+    }
+}
---
+++ src/main/java/org/appa/skippa/skript/elements/expressions/ExprDistinct.java
@@ -0,0 +1,79 @@
+package org.appa.skippa.skript.elements.expressions;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Example;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.util.LiteralUtils;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.jspecify.annotations.Nullable;
+import org.skriptlang.skript.registration.SyntaxInfo;
+import org.skriptlang.skript.registration.SyntaxRegistry;
+
+import java.util.Arrays;
+
+@Name("Distinct")
+@Description({"Returns the values of an expression with duplicate values removed."})
+@Example("""
+       on script load:
+           set {_t::*} to (all integers between 0 and 5)
+           add 5 to {_t::*}
+           broadcast {_t::*} without duplicates # 0,1,2,3,4,5
+       """)
+@Since("1.0")
+public class ExprDistinct extends SimpleExpression<Object> {
+
+    public static void register(SyntaxRegistry registry) {
+        registry.register(SyntaxRegistry.EXPRESSION, SyntaxInfo.Expression.builder(ExprDistinct.class, Object.class)
+                .supplier(ExprDistinct::new)
+                .addPatterns(
+                        "%objects% without duplicate[s] [values]",
+                        "[the] unique values of %objects%"
+                )
+                .build());
+    }
+
+
+    private Expression<?> objects;
+
+    @Override
+    public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        Expression<Object> expression = LiteralUtils.defendExpression(expressions[0]);
+
+        if (LiteralUtils.hasUnparsedLiteral(expression)) {
+            return false;
+        }
+
+        this.objects = expression;
+        return true;
+    }
+
+
+    @Override
+    protected Object @Nullable [] get(Event event) {
+        return Arrays.stream(objects.getArray(event))
+                .distinct()
+                .toArray();
+    }
+
+    @Override
+    public boolean isSingle() {
+        return this.objects.isSingle();
+    }
+
+
+    @Override
+    public String toString(@Nullable Event event, boolean debug) {
+        return "unique values of " + objects.toString(event, debug);
+    }
+
+
+    @Override
+    public Class<?> getReturnType() {
+        return Object.class;
+    }
+}
